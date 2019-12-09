842cb39c5b4332eb340582f3b168747eed4c9066
Porson <porsonlee@gmail.com>
2019-12-09 23:31:53 +0000

    added get urls after creating urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -20,3 +20,8 @@ app.listen(PORT, () => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
